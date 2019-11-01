d3cd48259f011c33fb8926dd4d4ba0a2825ec3be
Shana Wiltshire <shanawiltshire@gmail.com>
2019-11-01 00:25:40 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_push (arg1, arg2)
+arg1.push (arg2)
+end
+
+def using_unshift (arg1, arg2)
+  arg1.unshift (arg2)
+end
+
+def using_pop (array)
+  array.pop()
+end
+
+def pop_with_args (array)
+  array.pop(2)
+end
+  
+  def using_shift ["array"]
+    array.shift(1)
+  end
+  
+ def shift_with_args (array, arg2) 
+   array.shift(2)
+  end
+  
+  def using_concat (array, arg2)
+    array.concat (arg2)
+  end
+    
